mit-pdos/xv6-public

commit 19f65413bdf7553036f2c388552580905730060a

hold ptable.lock for the entire process creation however, processes still modify their own p->* without locking

proc.c
@@ -31,23 +31,21 @@ pinit(void)
 // If found, change state to EMBRYO and initialize
 // state required to run in the kernel.
 // Otherwise return 0.
+// Must hold ptable.lock.
 static struct proc*
 allocproc(void)
 {
   struct proc *p;
   char *sp;
 
-  acquire(&ptable.lock);
   for(p = ptable.proc; p < &ptable.proc[NPROC]; p++)
     if(p->state == UNUSED)
       goto found;
-  release(&ptable.lock);
   return 0;
 
 found:
   p->state = EMBRYO;
   p->pid = nextpid++;
-  release(&ptable.lock);
 
   // Allocate kernel stack.
   if((p->kstack = kalloc()) == 0){
@@ -81,6 +79,8 @@ userinit(void)
   struct proc *p;
   extern char _binary_initcode_start[], _binary_initcode_size[];
   
+  acquire(&ptable.lock);
+
   p = allocproc();
   initproc = p;
   if((p->pgdir = setupkvm()) == 0)
@@ -100,6 +100,8 @@ userinit(void)
   p->cwd = namei("/");
 
   p->state = RUNNABLE;
+
+  release(&ptable.lock);
 }
 
 // Grow current process's memory by n bytes.
@@ -131,15 +133,20 @@ fork(void)
   int i, pid;
   struct proc *np;
 
+  acquire(&ptable.lock);
+
   // Allocate process.
-  if((np = allocproc()) == 0)
+  if((np = allocproc()) == 0){
+    release(&ptable.lock);
     return -1;
+  }
 
   // Copy process state from p.
   if((np->pgdir = copyuvm(proc->pgdir, proc->sz)) == 0){
     kfree(np->kstack);
     np->kstack = 0;
     np->state = UNUSED;
+    release(&ptable.lock);
     return -1;
   }
   np->sz = proc->sz;
@@ -158,9 +165,8 @@ fork(void)
  
   pid = np->pid;
 
-  // lock to force the compiler to emit the np->state write last.
-  acquire(&ptable.lock);
   np->state = RUNNABLE;
+
   release(&ptable.lock);
   
   return pid;
@@ -233,11 +239,11 @@ wait(void)
         kfree(p->kstack);
         p->kstack = 0;
         freevm(p->pgdir);
-        p->state = UNUSED;
         p->pid = 0;
         p->parent = 0;
         p->name[0] = 0;
         p->killed = 0;
+        p->state = UNUSED;
         release(&ptable.lock);
         return pid;
       }

Makefile
@@ -74,8 +74,8 @@ AS = $(TOOLPREFIX)gas
 LD = $(TOOLPREFIX)ld
 OBJCOPY = $(TOOLPREFIX)objcopy
 OBJDUMP = $(TOOLPREFIX)objdump
-#CFLAGS = -fno-pic -static -fno-builtin -fno-strict-aliasing -O2 -Wall -MD -ggdb -m32 -Werror -fno-omit-frame-pointer
-CFLAGS = -fno-pic -static -fno-builtin -fno-strict-aliasing -fvar-tracking -fvar-tracking-assignments -O0 -g -Wall -MD -gdwarf-2 -m32 -Werror -fno-omit-frame-pointer
+CFLAGS = -fno-pic -static -fno-builtin -fno-strict-aliasing -O2 -Wall -MD -ggdb -m32 -Werror -fno-omit-frame-pointer
+#CFLAGS = -fno-pic -static -fno-builtin -fno-strict-aliasing -fvar-tracking -fvar-tracking-assignments -O0 -g -Wall -MD -gdwarf-2 -m32 -Werror -fno-omit-frame-pointer
 CFLAGS += $(shell $(CC) -fno-stack-protector -E -x c /dev/null >/dev/null 2>&1 && echo -fno-stack-protector)
 ASFLAGS = -m32 -gdwarf-2 -Wa,-divide
 # FreeBSD ld wants ``elf_i386_fbsd''

phf/xv6-public-fall2016

commit 2533a5de6c3ac935117a85190439118f4087d41e

Fix deadlock between fork() and icache.
This was caused by 19f65413bdf7553036f2c388552580905730060a's move to
holding ptable.lock across the entirety of process creation; we need
to drop it for the complex call chains inside the filesystem.

  (gdb) info threads
    Id   Target Id         Frame 
  * 1    Thread 1 (CPU#0 [running]) acquire (lk=0x80111a00 <icache>) at spinlock.c:32
    2    Thread 2 (CPU#1 [running]) acquire (lk=0x80113120 <ptable>) at spinlock.c:32
  (gdb) bt
  #0  acquire (lk=0x80111a00 <icache>) at spinlock.c:32
  #1  0x80101c14 in idup (ip=0x80111a34 <icache+52>) at fs.c:262
  #2  0x80104042 in fork () at proc.c:161
  #3  0x80104db9 in syscall () at syscall.c:132
  #4  0x80105dd9 in trap (tf=0x8dfbefb4) at trap.c:49
  #5  0x80105bcb in alltraps () at trapasm.S:23
  #6  0x8dfbefb4 in ?? ()
  Backtrace stopped: previous frame inner to this frame (corrupt stack?)
  (gdb) thread 2
  [Switching to thread 2 (Thread 2)]
  #0  acquire (lk=0x80113120 <ptable>) at spinlock.c:32
  32	  while(xchg(&lk->locked, 1) != 0)
  (gdb) bt
  #0  acquire (lk=0x80113120 <ptable>) at spinlock.c:32
  #1  0x80104574 in wakeup (chan=0x80111a34 <icache+52>) at proc.c:413
  #2  0x80101d74 in iunlock (ip=0x80111a34 <icache+52>) at fs.c:310
  #3  0x801023dd in namex (path=<optimized out>, nameiparent=1, name=0x8dfbcf12 "p0") at fs.c:627
  #4  0x80104e1f in create (path=<optimized out>, type=type@entry=2, major=major@entry=0, minor=0) at sysfile.c:245
  #5  0x80105614 in sys_open () at sysfile.c:297
  #6  0x80104db9 in syscall () at syscall.c:132
  #7  0x80105dd9 in trap (tf=0x8dfbcfb4) at trap.c:49
  #8  0x80105bcb in alltraps () at trapasm.S:23
  #9  0x8dfbcfb4 in ?? ()
  Backtrace stopped: previous frame inner to this frame (corrupt stack?)

proc.c
@@ -155,12 +155,22 @@ fork(void)
   // Clear %eax so that fork returns 0 in the child.
   np->tf->eax = 0;
 
+  safestrcpy(np->name, proc->name, sizeof(proc->name));
+  release(&ptable.lock);
+
+  // It is safe to release ptable.lock here and yet reference both
+  // np-> and proc-> objects because np-> is in state EMBRYO and
+  // therefore not moving until we say so, and proc-> is us, so,
+  // again, not doing anything with its ofile[]s or its cwd until
+  // we let it.
   for(i = 0; i < NOFILE; i++)
     if(proc->ofile[i])
       np->ofile[i] = filedup(proc->ofile[i]);
   np->cwd = idup(proc->cwd);
 
-  safestrcpy(np->name, proc->name, sizeof(proc->name));
+  // This acquire-set-release dance forces the compiler and the CPU
+  // to do and publish all of the above changes before this write
+  acquire(&ptable.lock);
 
   pid = np->pid;
 